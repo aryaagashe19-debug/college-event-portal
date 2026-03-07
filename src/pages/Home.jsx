d53d128922f972c2e9e6bd19ad1c5f2563e7e077
Arya <aryaagashe19@gmail.com>
2026-03-07 05:52:05 +0530

    Fixed center alignment

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
-<div className="home-container">
-  <h1>Welcome to College Event Portal 🎉</h1>
-  <p>Register for upcoming college events</p>
+function Home() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to College Event Portal 🎉</h1>
+      <p>Register for upcoming college events</p>
 
-  <div className="button-group">
-    <button className="btn-yellow">View Events</button>
-    <button className="btn-blue">Register Now</button>
-  </div>
-</div>
+      <div>
+        <button>View Events</button>
+        <button>Register Now</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
